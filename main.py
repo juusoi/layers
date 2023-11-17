e47286e48d07200d449a805dff3987e124b80ed6
Juuso Issakainen <7767433+juusoi@users.noreply.github.com>
2023-11-18 01:19:38 +0200

    better if

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ from layers import create_layers
 def main():
     while True:
         layers = input("How many layers (1-26), exit with 0: ")
-        if layers.isnumeric() and int(layers) > 0 and int(layers) <= 26:
+        if layers.isnumeric() and int(layers) == 0:
+            print("Exiting...")
+            break
+        elif layers.isnumeric() and 0 < int(layers) <= 26:
             layers = int(layers)
             print("You have chosen {} layers.".format(layers))
             print(create_layers(layers))
-        elif layers == "0":
-            print("Exiting...")
-            break
         else:
             print("Invalid input. Try again." + "\n")
 
